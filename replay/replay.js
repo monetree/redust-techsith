a028aec7447b66156fbe665a3585334eccd74307
soubhagya <soubhagya.pradhan@fractal.ai>
2019-07-28 14:46:34 +0530

    adding changres

---
+++ replay/replay.js
@@ -0,0 +1,26 @@
+const { createStore } = require('redux');
+
+const initialState = {
+    age:21
+}
+
+const myReducer = (state = initialState, action) => {
+    const newState = state
+    if(action.type === "ADD"){
+        newState.age += action.val;
+    }
+    if(action.type === "SUBSTRACT"){
+        newState.age -= action.val;
+    }
+
+    return newState;
+}
+
+const store = createStore(myReducer);
+store.subscribe(() => {
+    console.log("state changed: ", JSON.stringify(store.getState()))
+    console.log("--------------------------------------")
+})
+
+store.dispatch({type:'ADD', val:10})
+store.dispatch({type:'SUBSTRACT', val:5})
--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+        <div>Age: <span>{this.props.age}</span></div>
+        <button onClick={this.props.onAgeUp}>Age Up</button>
+        <button onClick={this.props.onAgeDown}>Age Down</button>
+        <hr/>
+        <div>History</div>
+        <div>
+          <ul>
+            {
+              this.props.history.map(el => (
+                <li
+                 key={el.id}
+                 onClick={() => this.props.onDeleteItem(el.id)}
+                 >
+                 {el.age}
+                </li>
+              ))
+
+            }
+          </ul>
+        </div>
+      </div>
+    );
+  }
+} 
+
+const mapStateToProps = (state) => {
+  return {
+    age: state.age,
+    history: state.history
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAgeUp: () => dispatch({type:'AGE_UP', val: 10}),
+    onAgeDown: () => dispatch({type:'AGE_DOWN', val: 5}),
+    onDeleteItem: (id) =>  dispatch({type:'DELETE_ITEM', key:id })
+  }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
